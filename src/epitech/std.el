16b01610ada8fd2507d19fe1383b0a78c4df3d8f
vanhec_a <alexandre.vanhecke@epitech.eu>
2018-09-24 18:25:03 +0200

    refactor: Switch from 8 column tab to 4 spaces indentation

--- src/epitech/std.el
+++ src/epitech/std.el
@@ -16,6 +16,7 @@
 
 ; Adjust indentation settings
 (setq c-default-style "linux"
-      c-basic-offset 8
-      tab-width 8
-      indent-tabs-mode t)
+      c-basic-offset 4
+      tab-width 4
+      indent-tabs-mode nil)
+(setq tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76 80))
