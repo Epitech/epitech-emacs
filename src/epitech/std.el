3582b5f4dde65b45c298e142b01363ce9449eb7d
Marc Planard <marc.planard@epitech.eu>
2018-10-03 12:08:57 +0200

    fix indentation (no more tabs)

--- src/epitech/std.el
+++ src/epitech/std.el
@@ -17,6 +17,6 @@
 ; Adjust indentation settings
 (setq c-default-style "linux"
       c-basic-offset 4
-      tab-width 4
-      indent-tabs-mode nil)
+      tab-width 4)
 (setq tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76 80))
+(setq-default indent-tabs-mode nil)
